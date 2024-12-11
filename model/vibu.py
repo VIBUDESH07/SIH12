5aa7e83b1c64053a457d724a98cc3b436fabaa36
VIBUDESH BALAJI <125853344+VIBUDESH07@users.noreply.github.com>
2024-12-11 19:27:23 +0530

    Change to deploy on aws

--- model/vibu.py
+++ model/vibu.py
@@ -145,4 +145,5 @@ def handle_disconnect():
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000)
+    socketio.run(app, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True)
 
