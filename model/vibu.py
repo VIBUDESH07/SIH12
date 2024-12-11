3fdcc6f8d9e448be4b9ee797411082fdc48f2a8f
VIBUDESH BALAJI <125853344+VIBUDESH07@users.noreply.github.com>
2024-12-11 19:45:08 +0530

    production server

--- model/vibu.py
+++ model/vibu.py
@@ -144,6 +144,6 @@ def handle_disconnect():
     print(f"Client {client_id} disconnected")
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+    # socketio.run(app, host="0.0.0.0", port=5000)
     socketio.run(app, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True)
 
